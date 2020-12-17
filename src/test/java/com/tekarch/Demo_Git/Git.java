988b0b99755c467ade881dd00a018719b49bb671
srinidhitekarch <srinidhi.b@tekarch.com>
2020-12-18 00:21:00 +0530

    message text

--- src/test/java/com/tekarch/Demo_Git/Git.java
+++ src/test/java/com/tekarch/Demo_Git/Git.java
@@ -5,10 +5,30 @@ public class Git {
 	/*
 	 If you have to store your projects or group of files --> google drive --> GIT
 	 
-	 
+	 1. install git to your local machine
+	 2. Create one github account
+	 	create an repository in git hub
+	 	copy the url of the repo
+	 	 
 	 git init
 	 git status
 	 git remote add origin <Repo URL>
 	 
+	 create .gitignore file using eclipse and add all unwanted files in to it.
+.classpath
+.project
+.settings/*
+desktop.ini
+target/*
+
+	git add (* or File name) --> this will add it to the ready state.
+	git commit -m "message"
+	git config --global user.email "you@example.com"
+	git config --global user.name "Your Name"
+
+	git push origin master
+	
+	
+	
 	 * */
 }
