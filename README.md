a30813c73b0bdea69d465440f60b953a8d73a81d
srinidhitekarch <srinidhi.b@tekarch.com>
2020-12-18 00:06:30 +0530

    First commit after creating project

---
+++ README.md
@@ -0,0 +1,1 @@
+# This project refers to GIT Concepts
---
+++ src/test/java/com/tekarch/Demo_Git/Git.java
@@ -0,0 +1,14 @@
+package com.tekarch.Demo_Git;
+
+public class Git {
+
+	/*
+	 If you have to store your projects or group of files --> google drive --> GIT
+	 
+	 
+	 git init
+	 git status
+	 git remote add origin <Repo URL>
+	 
+	 * */
+}
